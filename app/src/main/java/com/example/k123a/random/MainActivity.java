8ae034f95c6617b24096db1f234947a98dd48138
AndreyK2001 <K123a2013@yandex.ru>
2017-11-13 19:38:00 +0300

    Added add function and sub function

--- app/src/main/java/com/example/k123a/random/MainActivity.java
+++ app/src/main/java/com/example/k123a/random/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.k123a.random;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void plus(View view) {
+        EditText num1 = (EditText) this.findViewById(R.id.num1);
+        String s_num1 = num1.getText().toString();
+        EditText num2 = (EditText) this.findViewById(R.id.num2);
+        String s_num2 = num2.getText().toString();
+        TextView out = (TextView) this.findViewById(R.id.out);
+        out.setText(""+(Double.parseDouble(s_num1)+Double.parseDouble(s_num2)));
+    }
+
+    public void minus(View view) {
+        EditText num1 = (EditText) this.findViewById(R.id.num1);
+        String s_num1 = num1.getText().toString();
+        EditText num2 = (EditText) this.findViewById(R.id.num2);
+        String s_num2 = num2.getText().toString();
+        TextView out = (TextView) this.findViewById(R.id.out);
+        out.setText(""+(Double.parseDouble(s_num1)-Double.parseDouble(s_num2)));
     }
 }
