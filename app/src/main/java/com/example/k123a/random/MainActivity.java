cd4a7cdf4e69021861545132ca8e50b947dcb767
AndreyK2001 <K123a2013@yandex.ru>
2017-11-17 18:35:09 +0300

    Added add function and sub function

--- app/src/main/java/com/example/k123a/random/MainActivity.java
+++ app/src/main/java/com/example/k123a/random/MainActivity.java
@@ -31,4 +31,21 @@ public class MainActivity extends AppCompatActivity {
         TextView out = (TextView) this.findViewById(R.id.out);
         out.setText(""+(Double.parseDouble(s_num1)-Double.parseDouble(s_num2)));
     }
+
+    public void mul(View view) {EditText num1 = (EditText) this.findViewById(R.id.num1);
+        String s_num1 = num1.getText().toString();
+        EditText num2 = (EditText) this.findViewById(R.id.num2);
+        String s_num2 = num2.getText().toString();
+        TextView out = (TextView) this.findViewById(R.id.out);
+        out.setText(""+(Double.parseDouble(s_num1)*Double.parseDouble(s_num2)));
+    }
+
+    public void del(View view) {
+        EditText num1 = (EditText) this.findViewById(R.id.num1);
+        String s_num1 = num1.getText().toString();
+        EditText num2 = (EditText) this.findViewById(R.id.num2);
+        String s_num2 = num2.getText().toString();
+        TextView out = (TextView) this.findViewById(R.id.out);
+        out.setText(""+(Double.parseDouble(s_num1)/Double.parseDouble(s_num2)));
+    }
 }
